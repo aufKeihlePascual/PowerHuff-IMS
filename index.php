4f3ed614d154dc9ca18a9575963295c34c3a3073
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-11-21 10:59:40 +0800

    Added Dashboard Routes for default and dynamic content loading

--- index.php
+++ index.php
@@ -8,21 +8,21 @@ global $conn;
 try {
     $router = new \Bramus\Router\Router();
 
-    # LOGIN - Starting Page
     $router->get('/', '\App\Controllers\HomeController@index');
 
     $router->get('/login', '\App\Controllers\LoginController@showLoginForm');
     $router->post('/login', '\App\Controllers\LoginController@login');
     $router->get('/logout', '\App\Controllers\LoginController@logout');
 
-    # Welcome Dashboard
-    $router->get('/welcome', '\App\Controllers\WelcomeController@showWelcome');
+    # Dashboard
+    $router->get('/dashboard', '\App\Controllers\DashboardController@showDashboard');
+    $router->get('/dashboard/content', '\App\Controllers\DashboardController@loadContent');
 
     $router->run();
-} catch (Exception $e) {
-    echo json_encode([
-        'error' => $e->getMessage()
-    ]);
-}
+    } catch (Exception $e) {
+        echo json_encode([
+            'error' => $e->getMessage()
+        ]);
+    }
 
 ?>

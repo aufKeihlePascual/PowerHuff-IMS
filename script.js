465b22d949a466ba0aa35ea7f847307030b31947
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-11-22 21:56:30 +0800

    Reverting "Merge pull request #1 from aufKeihlePascual/Dashboard-UI"
    This reverts commit cda9f45b51b03fa7b672f35d3952d62fac48363d, reversing
    changes made to 0f4a3c25723eacb1584e5b179ef007d9d12719ae.

--- script.js
+++
@@ -1,6 +0,0 @@
-// Example: Alert when a card is clicked
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert('Card clicked!');
-    });
-});

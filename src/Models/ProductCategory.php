114dcb80a1472f929daa9590bbd4f37bf59abcde
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-12-07 21:29:20 +0800

    Added ProductCategory model

---
+++ src/Models/ProductCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class Product extends BaseModel
+{
+        public function getProductsByCategory($category_id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM products WHERE Category_ID = :category_id");
+        $stmt->bindParam(':category_id', $category_id);
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    
+   
+}
